0506b0245de441a648be007f97d57c4a36fc6501
Patrick Smith <pgwsmith@gmail.com>
2024-08-23 19:44:52 +1000

    Add more functions to Gregorian

--- lib/silver_orb/calendar/gregorian.ex
+++ lib/silver_orb/calendar/gregorian.ex
@@ -1,6 +1,19 @@
 defmodule SilverOrb.Calendar.Gregorian do
   use Orb
 
+  defw leap_year?(year: I32), I32 do
+    I32.rem_u(year, 4) === 0 &&& (I32.rem_u(year, 100) !== 0 or I32.rem_u(year, 400) === 0)
+  end
+
+  defw days_in_month(year: I32, month: I32), I32 do
+    if month === 2, do: return(28 + leap_year?(year))
+
+    [4, 6, 9, 11]
+    |> Enum.map(fn m -> m === month end)
+    |> Enum.reduce(&I32.or/2)
+    |> if(do: i32(30), else: i32(31))
+  end
+
   defw day_of_week(year: I32, month: I32, day: I32), I32, k: I32, j: I32, weekday: I32 do
     if month < 3 do
       month = month + 12
--- test/gregorian_calendar_test.exs
+++ test/gregorian_calendar_test.exs
@@ -50,6 +50,38 @@ defmodule GregorianCalendarTest do
   #   |> Stream.run()
   # end
 
+  test "leap_year?", %{instance: instance} do
+    assert 1 = Instance.call(instance, "leap_year?", 2000)
+    assert 0 = Instance.call(instance, "leap_year?", 2001)
+    assert 0 = Instance.call(instance, "leap_year?", 2015)
+    assert 1 = Instance.call(instance, "leap_year?", 2016)
+    assert 1 = Instance.call(instance, "leap_year?", 2020)
+    assert 0 = Instance.call(instance, "leap_year?", 2100)
+
+    1700..3000
+    |> Enum.take_random(1234)
+    |> Enum.each(fn year ->
+      expected = if Calendar.ISO.leap_year?(year), do: 1, else: 0
+
+      assert expected === Instance.call(instance, "leap_year?", year)
+    end)
+  end
+
+  test "days_in_month", %{instance: instance} do
+    assert 31 = Instance.call(instance, "days_in_month", 2000, 1)
+    assert 29 = Instance.call(instance, "days_in_month", 2000, 2)
+    assert 31 = Instance.call(instance, "days_in_month", 2000, 3)
+    assert 30 = Instance.call(instance, "days_in_month", 2000, 4)
+    assert 28 = Instance.call(instance, "days_in_month", 2100, 2)
+
+    for(year <- 1700..3000, month <- 1..12, do: {year, month})
+    |> Enum.each(fn {year, month} ->
+      expected = Calendar.ISO.days_in_month(year, month)
+
+      assert expected === Instance.call(instance, "days_in_month", year, month)
+    end)
+  end
+
   test "day_of_week", %{instance: instance} do
     assert 1 = Instance.call(instance, "day_of_week", 2016, 10, 31)
     assert 2 = Instance.call(instance, "day_of_week", 2016, 11, 1)
@@ -63,10 +95,11 @@ defmodule GregorianCalendarTest do
     end_date = ~D[3000-01-10]
 
     Date.range(start_date, end_date)
+    |> Enum.take_random(1234)
     |> Enum.each(fn date ->
       expected_weekday = Date.day_of_week(date)
 
-      assert expected_weekday =
+      assert expected_weekday ===
                Instance.call(instance, "day_of_week", date.year, date.month, date.day)
     end)
   end
