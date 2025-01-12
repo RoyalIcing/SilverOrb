d247206cbed6dd7bc53b1b645fa1a35bbf32e2a4
Patrick Smith <pgwsmith@gmail.com>
2025-01-12 20:17:54 +1100

    Allow generating ICO files

---
+++ lib/silver_orb/ico.ex
@@ -0,0 +1,79 @@
+defmodule SilverOrb.ICO do
+  use Orb
+
+  Memory.pages(2)
+
+  defwp write_header(write_ptr: I32.UnsafePointer, count: I32), I32.UnsafePointer do
+    Memory.store!(I32.U16, write_ptr, 0)
+    Memory.store!(I32.U16, write_ptr + 2, 1)
+    Memory.store!(I32.U16, write_ptr + 4, count)
+    write_ptr + 6
+  end
+
+  defwp write_image_directory(
+          write_ptr: I32.UnsafePointer,
+          width: I32,
+          height: I32,
+          image_offset: I32
+        ),
+        I32.UnsafePointer do
+    Memory.store!(I32.U8, write_ptr, width)
+    Memory.store!(I32.U8, write_ptr + 1, height)
+    Memory.store!(I32.U8, write_ptr + 2, 0)
+    Memory.store!(I32.U8, write_ptr + 3, 0)
+    Memory.store!(I32.U16, write_ptr + 4, 1)
+    Memory.store!(I32.U16, write_ptr + 6, 32)
+    Memory.store!(I32, write_ptr + 8, width * height * 4 + 40)
+    Memory.store!(I32, write_ptr + 12, image_offset)
+    write_ptr + 16
+  end
+
+  defwp write_image_data(write_ptr: I32.UnsafePointer, width: I32, height: I32, color: I32),
+    y: I32,
+    x: I32 do
+    Memory.store!(I32, write_ptr, 40)
+    Memory.store!(I32, write_ptr + 4, width)
+    Memory.store!(I32, write_ptr + 8, height * 2)
+    Memory.store!(I32.U16, write_ptr + 12, 1)
+    Memory.store!(I32.U16, write_ptr + 14, 32)
+    Memory.store!(I32, write_ptr + 16, 0)
+    Memory.store!(I32, write_ptr + 20, width * height)
+    Memory.store!(I32, write_ptr + 24, 0)
+    Memory.store!(I32, write_ptr + 28, 0)
+    Memory.store!(I32, write_ptr + 32, 0)
+    Memory.store!(I32, write_ptr + 36, 0)
+
+    write_ptr = write_ptr + 40
+
+    loop Rows do
+      x = 0
+
+      loop Columns do
+        # Memory.store!(I32, write_ptr + ((height - y - 1) * width + x) * 4, color)
+        Memory.store!(I32.U8, write_ptr + ((height - y - 1) * width + x) * 4 + 0, 0)
+        Memory.store!(I32.U8, write_ptr + ((height - y - 1) * width + x) * 4 + 1, 255)
+        Memory.store!(I32.U8, write_ptr + ((height - y - 1) * width + x) * 4 + 2, 0)
+        Memory.store!(I32.U8, write_ptr + ((height - y - 1) * width + x) * 4 + 3, 255)
+
+        x = x + 1
+
+        if x < width do
+          Columns.continue()
+        end
+      end
+
+      y = y + 1
+
+      if y < height do
+        Rows.continue()
+      end
+    end
+  end
+
+  defw write(write_ptr: I32.UnsafePointer, width: I32, height: I32, color: I64), I32 do
+    write_ptr = write_header(write_ptr, 1)
+    write_ptr = write_image_directory(write_ptr, width, height, 6 + 16)
+    write_image_data(write_ptr, width, height, I32.wrap_i64(color))
+    6 + 16 + 40 + width * height * 4
+  end
+end
---
+++ test/ico_test.exs
@@ -0,0 +1,47 @@
+defmodule ICOTest do
+  use ExUnit.Case, async: true
+
+  setup do
+    wat = Orb.to_wat(SilverOrb.ICO)
+    %{wat: wat}
+  end
+
+  setup %{wat: wat} do
+    {:ok, pid} = Wasmex.start_link(%{bytes: wat})
+    {:ok, memory} = Wasmex.memory(pid)
+    {:ok, store} = Wasmex.store(pid)
+
+    call_function = &Wasmex.call_function(pid, &1, &2)
+    read_binary = &Wasmex.Memory.read_binary(store, memory, &1, &2)
+    write_binary = &Wasmex.Memory.write_binary(store, memory, &1, &2)
+
+    %{
+      pid: pid,
+      memory: memory,
+      store: store,
+      call_function: call_function,
+      read_binary: read_binary,
+      write_binary: write_binary
+    }
+  end
+
+  test "write pink ico file", %{call_function: call_function, read_binary: read_binary} do
+    path = Path.join(__DIR__, "pink.ico")
+
+    write_ptr = Orb.Memory.page_byte_size()
+    # {:ok, [byte_count]} = call_function.(:write, [write_ptr, 16, 16, 0x000000FF - 0x80000000])
+    {:ok, [byte_count]} = call_function.(:write, [write_ptr, 16, 16, 0xFFFF0000])
+
+    bytes = read_binary.(write_ptr, byte_count)
+    File.write!(path, bytes)
+  end
+
+  defp u32_to_s32(int) do
+    if int <= 0x7FFF_FFFF do
+      int
+    else
+      int - 0x1_0000_0000
+    end
+    # Bitwise.band(int, 0x7FFF_FFFF) - if int > 0x7FFF_FFFF, do: 1, else: 0
+  end
+end
